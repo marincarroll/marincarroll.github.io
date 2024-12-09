35ba7b91d7e624fa9443d549f7cf7179fc6a452b
Marin @Home <marin@marincarroll.com>
2024-12-09 10:43:31 -0500

    adds strict a11y to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,5 @@
 import eslintPluginAstro from "eslint-plugin-astro";
-export default eslintPluginAstro.configs.recommended;
+export default [
+  ...eslintPluginAstro.configs['recommended'],
+  ...eslintPluginAstro.configs['jsx-a11y-strict'],
+];
